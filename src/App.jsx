3fc3afb5453b0c98f3d7fe58de429728c89321ff
Manavk2004 <mjkamdar04@gmail.com>
2025-09-26 16:35:01 -0400

    LanguageBar Added

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,19 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import Navigation from './modules/ui/components/NavBar'
 import LeftSide from './modules/ui/HomePage/Left-side'
 import RightSide from './modules/ui/HomePage/Right-side'
+import LanguageBar from './modules/ui/components/LanguageBar'
 
 function App() {
   return (
     <>
-    <main className='bg-[#1a1823] h-screen w-screen flex flex-col '>
+    <main className='bg-[#1a1823]/98 h-full w-screen flex flex-col'>
       <Navigation/>
-      <div className='flex flex-grow items-center ml-15 mr-10 justify-between gap-100 h-screen flex-1'>
+      <div className='flex  items-center ml-15 mr-10 justify-between gap-100 h-screen'>
         <LeftSide />
         <RightSide />
+      </div>
+      <div>
+        <LanguageBar />
       </div>
     </main>
     </>
