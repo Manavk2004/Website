f5445468c3592191d9aa3ca0a0960db762279897
Manavk2004 <mjkamdar04@gmail.com>
2025-09-27 14:07:23 -0400

    skills bars base done

--- src/modules/ui/components/TechnologyBar.jsx
+++ src/modules/ui/components/TechnologyBar.jsx
@@ -10,15 +10,11 @@ function TechnologyBar() {
   return (
       <div className='w-full bg-[#1a1823] text-white  h-64 flex justify-center items-center'>
         <img src={node} className='grayscale opacity-20 size-2/5 hover:opacity-60 transition-opacity ease-out duration-300'/>
-        <img src={next} className=' opacity-100 size-2/5 transition-opacity ease-out duration-300'/>
+        <img src={next} className=' grayscale opacity-20 size-2/5 transition-opacity hover:opacity-60 ease-out duration-300'/>
         <img src={express} className=' grayscale opacity-30 size-2/5 hover:opacity-60 transition-opacity ease-out duration-300'/>
         <img src={react} className=' grayscale opacity-30 size-2/5 hover:opacity-60 transition-opacity ease-out duration-300'/>
         <img src={sql} className=' grayscale opacity-30 size-2/5 hover:opacity-60 transition-opacity ease-out duration-300'/>
         <img src={fastapi} className=' grayscale opacity-30 size-2/5 hover:opacity-60 transition-opacity ease-out duration-300'/>
-
-
-
-
       </div>
   )
 }
